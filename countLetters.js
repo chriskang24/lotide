64f07bce169b0d63dad227ea2a0ee1f9a3233533
Christopher Kang <christopherjkang24@gmail.com>
2021-01-06 23:07:37 +0000

    countletters completed

---
+++ countLetters.js
@@ -0,0 +1,29 @@
+const assertEqual = function (actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+function countLetters(string) {
+
+  let moddedString = string.split("");
+  let countList = {};
+
+  for (let character of moddedString) {
+    if (character !== " ") {
+      if (countList[character]) {
+        countList[character] += 1;
+      } else {
+        countList[character] = 1;
+      }
+    }
+  }
+
+  return countList;
+};
+
+countLetters("lighthouse in the house")
