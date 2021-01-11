b0d739fdd05f75435735a913feeb8c85da3aef13
chriskang24 <christopherjkang24@gmail.com>
2021-01-11 15:12:17 -0500

    updated to include chai + mocha functionality

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,20 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
 const middle = require('../middle');
 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [ 3, 4 ]);
+describe("#middle", () => { 
+
+it("should return the two middle values '(3, 4)' when passed an array of even length", () => {
+  assert.deepStrictEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+});
+
+it("should return the one middle value '3' when passed an array of odd length", () => {
+  assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+});
+
+it ("should return an empty array '[]' when passed an array with one or two elements", () => {
+  assert.deepStrictEqual(middle([1, 2]), []);
+});
+
+
+});
