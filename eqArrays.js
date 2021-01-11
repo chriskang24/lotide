dd49abf6243f58bab6699033453074213681d0bb
chriskang24 <christopherjkang24@gmail.com>
2021-01-11 11:34:13 -0500

    eqArrays.js refactored to include export

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,4 @@
-function assertEqual(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 function eqArrays(firstComparison, secondComparison) {
 
@@ -21,15 +13,17 @@ function eqArrays(firstComparison, secondComparison) {
     console.log("first comparison for loop: ", firstComparison[i])
     console.log("second comparison for loop: ", secondComparison[i])
 
-      if (firstComparison[i] !== secondComparison[i]) {
-        return false;
-    } 
+    if (firstComparison[i] !== secondComparison[i]) {
+      return false;
+    }
   }
-    return true;
-  };
+  return true;
+};
 
 
 
 // console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]))
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+module.exports = eqArrays;
