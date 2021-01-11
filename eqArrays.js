c2f8a844ada84cf7aa67cd81ee63b42f58982d60
chriskang24 <christopherjkang24@gmail.com>
2021-01-11 11:39:27 -0500

    eqArrays.js refactored to include export

--- eqArrays.js
+++ eqArrays.js
@@ -2,7 +2,7 @@ const assertEqual = require('./assertEqual');
 
 function eqArrays(firstComparison, secondComparison) {
 
-  console.log(firstComparison.length === secondComparison.length)
+  // console.log(firstComparison.length === secondComparison.length)
 
   if (firstComparison.length !== secondComparison.length) {
     return false;
@@ -10,8 +10,8 @@ function eqArrays(firstComparison, secondComparison) {
 
   for (let i = 0; i < firstComparison.length; i++) {
 
-    console.log("first comparison for loop: ", firstComparison[i])
-    console.log("second comparison for loop: ", secondComparison[i])
+    // console.log("first comparison for loop: ", firstComparison[i])
+    // console.log("second comparison for loop: ", secondComparison[i])
 
     if (firstComparison[i] !== secondComparison[i]) {
       return false;
